a9debab6358d4b2ea4edf74a47cb02d80d31e73d
Karl Freund <karl.freund@placesense.ai>
2022-11-15 12:34:20 +0200

    exercises 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,37 +1,50 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content exercises = {[exercises1,exercises2,exercises3]} part={[part1,part2,part3]} />
-      <Total exercises = {[exercises1 , exercises2 , exercises3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />   
     </div>
   )
 }
 
+// title
 const Header = (props) =>{
 return(
   <h1>{props.course}</h1>
 )
 }
 
+// Content of all courses
 const Content = (props) =>{
   return(
     <div>
-      <Part part={props.part[0]} exercises={props.exercises[0]} />
-      <Part part={props.part[1]} exercises={props.exercises[1]} />
-      <Part part={props.part[2]} exercises={props.exercises[2]} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
 
+// parts of content
 const Part = (props) =>{
   return(
     <p> {props.part} {props.exercises} </p>
@@ -39,10 +52,10 @@ const Part = (props) =>{
 
 }
 
-
+// calculate total of all exercises
 const Total = (props) =>{
   return(
-    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
