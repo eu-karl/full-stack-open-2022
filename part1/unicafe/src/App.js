e465c1d88090c4fbc9264e14dc99cfb6dbc247aa
Karl <68251820+ka-rl@users.noreply.github.com>
2022-11-29 18:25:57 +0200

    exercise 1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,14 +15,16 @@ const Statistics = ({good,neutral,bad}) => {
     const positive = good/all
     if(all){
         return(
-            <div>
-                <StatisticsLine text={"good"} value={good}/>
-                <StatisticsLine text={"neutral"} value={neutral}/>
-                <StatisticsLine text={"bad"} value={bad}/>
-                <StatisticsLine text={"all"} value={all}/>all
-                <StatisticsLine text={"average"} value={average}/>
-                <StatisticsLine text={"positive"} value={positive}/>
-            </div>
+            <table>
+                <tbody>
+                    <StatisticsLine text={"good"} value={good}/>
+                    <StatisticsLine text={"neutral"} value={neutral}/>
+                    <StatisticsLine text={"bad"} value={bad}/>
+                    <StatisticsLine text={"all"} value={all}/>
+                    <StatisticsLine text={"average"} value={average}/>
+                    <StatisticsLine text={"positive"} value={positive}/>
+                </tbody>
+            </table>
         )
     }
     else return(
@@ -32,7 +34,7 @@ const Statistics = ({good,neutral,bad}) => {
     )
 }
 
-const StatisticsLine = ({text, value}) => <div>{text}: {value}</div>
+const StatisticsLine = ({text, value}) => <tr><td>{text}:</td><td>{value}</td></tr>
 
 
 const App = () => {
