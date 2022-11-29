c926c81e234214e44974ed1b60e0a06225ee5a9f
Karl <68251820+ka-rl@users.noreply.github.com>
2022-11-29 17:41:52 +0200

    exercise 1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,49 @@
-import {useState} from "react";
+import { useState } from 'react'
 
-const Button = (props) => (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-)
+// dynamic button, takes as args: its text, the function to be called for state change, the state to be changed
+const Button = (props) => {
+    return(
+        <button onClick={props.handleClick}>
+            {props.text}
+        </button>
+    )
+}
+
+const StatisticsDisplay = ({good,neutral,bad}) => {
+    return(
+        <div>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+        </div>
+    )
+}
+
 
 const App = () => {
-  const [value, setValue] = useState(10)
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
+    // returns function
+    const increment = (fun,value) => {
+        console.log(value)
+        return(() => fun(value + 1))
+    }
 
-  // Do not define components inside another component
-  const Display = props => <div>{props.value}</div>
-
-  return (
-      <div>
-        <Display value={value} />
-        <Button handleClick={() => setToValue(1000)} text="thousand" />
-        <Button handleClick={() => setToValue(0)} text="reset" />
-        <Button handleClick={() => setToValue(value + 1)} text="increment" />
-      </div>
-  )
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <div>
+                <Button handleClick={increment(setGood,good)} text={"good"}></Button>
+                <Button handleClick={increment(setNeutral,neutral)} text={"neutral"}></Button>
+                <Button handleClick={increment(setBad,bad)} text={"bad"}></Button>
+            </div>
+            <h1>statistics</h1>
+            <StatisticsDisplay good={good} neutral={neutral} bad={bad}></StatisticsDisplay>
+        </div>
+    )
 }
+
+export default App
