e235f642645d90b45a7a1558059aad04523ac479
Karl <68251820+ka-rl@users.noreply.github.com>
2022-12-01 18:52:46 +0200

    update exercise 1.14*
    as far as i got

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -21,10 +21,43 @@ const Button = (props) =>{
         return <div>Error displaying the button</div>
 }
 
+const DisplayBest = (props) =>{
+    let copy ={...props.votes}
+    let index = indexOfMax(copy)
+    props.setBest(props.anecdotes[index])
+
+    return(
+        <div>
+            {props.best} <br/>
+            has {copy[index]} votes.
+        </div>
+    )
+}
+
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        debugger
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            debugger
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
 }
 
 const App = () => {
@@ -37,17 +70,20 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+    const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+    const [best, setBest] = useState("No votes yet!")
 
-  const [selected, setSelected] = useState(0)
-
-  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
-
-  return (
+    return (
       <div>
-        {anecdotes[selected]} <br/>
-        has {votes[selected]} votes <br/>
-        <Button handleClickFunction={setSelected} max={anecdotes.length-1} text={"another one"}/>
-        <Button handleClickFunction={setVotes} text={"upvote"} votes={votes} selected={selected}></Button>
+          <h1> Anecdotes of the day</h1>
+          {anecdotes[selected]} <br/>
+          has {votes[selected]} votes <br/>
+          <Button handleClickFunction={setSelected} max={anecdotes.length-1} text={"another one"}/>
+          <Button handleClickFunction={setVotes} text={"upvote"} votes={votes} selected={selected}></Button>
+
+          <h1>Best anecdote</h1>
+          <DisplayBest votes = {votes} anecdotes={anecdotes} setBest={setBest} best={best} ></DisplayBest>
       </div>
   )
 }
